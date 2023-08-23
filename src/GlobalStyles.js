71d69d5bf9f65c762f009664070ef4689c1b48d7
Artur Mochnia <amochnia@gmail.com>
2023-08-23 09:41:08 +0100

    update GlobalStyle

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -11,6 +11,15 @@ export const GlobalStyles = createGlobalStyle`
     box-sizing: inherit;
   }
 
+  body {
+  font-family: 'Poppins', sans-serif;
+  margin: 0;
+  word-break: break-word;
+  background: ${({ theme }) => theme.colors.whisper};
+  padding: 16px;
+  color: ${({ theme }) => theme.colors.black};
+}
+
   #root {
     font-family: "Poppins", sans-serif;
     background-color: ${({ theme }) => theme.colors.whisper};
