1cee3a669f16b6ca5e0508150232cff19eca6e75
Artur Mochnia <amochnia@gmail.com>
2023-08-23 10:33:21 +0100

    update browserRouter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import MoviesBrowser from "./features/MoviesBrowser";
 import Navigation from "./common/Navigation";
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
 
-const App = () => 
-<Navigation />;
-<MoviesBrowser />;
+const App = () => (
+  <BrowserRouter>
+    <div>
+      <Navigation />
+      <MoviesBrowser />
+    </div>
+  </BrowserRouter>
+);
 
 export default App;
