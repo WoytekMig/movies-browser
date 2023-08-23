362064cea3df6837f68c599d8c61ab1575f6494c
Artur Mochnia <amochnia@gmail.com>
2023-08-23 09:41:29 +0100

    update theme

--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,13 @@
 export const theme = {
   colors: {
-    whisper: "#F5F5FA",
+    white: "#ffffff",
+    whisper: "#f5f5fa",
+    black: "#000000",
+    mystic: "#e4e6f0",
+    woodSmoke: "#18181B",
+    waterloo: "#7E839A",
+  },
+  breakpoint: {
+    mobileMax: 768,
   },
 };
