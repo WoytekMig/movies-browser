26ab3621c42a73b4b66a6924d5a4eddadfe09de1
Artur Mochnia <amochnia@gmail.com>
2023-08-24 19:24:12 +0100

    deleted file

--- src/routes.js
+++
@@ -1,4 +0,0 @@
-export const toMovies = () => '/movies';
-export const toPeople = () => '/people';
-export const toMovie = ({ movieId = ':movieId' }) => `/movies/${movieId}`;
-export const toPerson = ({ personId = ':personId' }) => `/people/${personId}`;
