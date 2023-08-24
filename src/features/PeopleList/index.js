db48994435fe9d7fe209a71ab96fa1fa0d188523
Artur Mochnia <amochnia@gmail.com>
2023-08-24 14:47:33 +0100

    added list styled

--- src/features/PeopleList/index.js
+++ src/features/PeopleList/index.js
@@ -1,12 +1,19 @@
 import { StyledContainer } from "../../common/Container/styled";
 import { MainHeader } from "../../common/MainHeader";
+import { List, ListItem, StyledLink } from "./styled";
+import PersonTile from "../../common/PersonTile"
 
 export const PeopleList = () => {
   return (
     <StyledContainer>
       <MainHeader title={"Popular People"} />
+      <List>
+        <ListItem>
+          <StyledLink>
+            <PersonTile />
+          </StyledLink>
+        </ListItem>
+      </List>
     </StyledContainer>
-      
-    
   );
 };
