df8ce8b0bf0037c52c923f9828f094819c08ea6b
Wojciech Migdał <122496250+WoytekMig@users.noreply.github.com>
2023-08-24 10:58:59 +0200

    added theme

--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,14 @@
 export const theme = {
   colors: {
+    black: "#000000",
+    mystic: "#e4e6f0",
+    woodSmoke: "#18181B",
     whisper: "#F5F5FA",
+    waterloo: "#7E839A",
+    white: "#FFFFFF",
+  },
+  common: {
+    boxShadow: "0px 4px 12px 0px #BAC7D580",
+    breakpoint: "767px",
   },
 };
