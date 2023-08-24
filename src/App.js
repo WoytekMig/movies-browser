85930aa06cca8b7621a6efc7557cd56f0293df74
Artur Mochnia <amochnia@gmail.com>
2023-08-24 11:38:04 +0100

    update container

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import MoviesBrowser from "./features/MoviesBrowser";
 import Navigation from "./common/Navigation";
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { PeopleList } from "./features/PeopleBrowser/PeopleList/index";
 
 const App = () => (
   <BrowserRouter>
     <div>
       <Navigation />
+      </div>
+      <div>
+      <PeopleList />Popular People
+      </div>
       <MoviesBrowser />
-    </div>
+    
   </BrowserRouter>
 );
 
--- src/common/Container/styled.js
+++ src/common/Container/styled.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
+export const StyledContainer = styled.div`
     max-width: 1400px;
     margin: auto;
     padding: 0 16px;
