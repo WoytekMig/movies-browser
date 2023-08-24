72cc7afd02d0e97995b1c81a69f83521ee0623e6
Artur Mochnia <amochnia@gmail.com>
2023-08-24 14:12:52 +0100

    update container

--- src/common/Container/index.js
+++ src/common/Container/index.js
@@ -1,5 +1,3 @@
 import { StyledContainer } from "./styled";
 
-export const MainContainer = ({ content }) => (
-    <StyledContainer>{content}</StyledContainer>
-);
+export const MainContainer = () => <StyledContainer />;
