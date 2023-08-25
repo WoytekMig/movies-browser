6d9649bc99896b58291ce0b1e0a4387e63798570
Artur Mochnia <amochnia@gmail.com>
2023-08-25 21:05:39 +0100

    added buttons

--- src/common/Pagination/index.js
+++ src/common/Pagination/index.js
@@ -1,28 +1,39 @@
-import { ArrowLeft, Button, ButtonText, PageCounter, PageNumber, Wrapper } from "./styled";
+import {
+  ArrowLeft,
+  ArrowRight,
+  Button,
+  ButtonText,
+  PageCounter,
+  PageNumber,
+  Wrapper,
+} from "./styled";
 
-const Pagination = ({currentPage, totalPages}) => {
-  
-  return(
-  <Wrapper>
-    <Button>
-      <ArrowLeft />
-      <ButtonText>First</ButtonText>
-    </Button>
-    <Button>
-      <ArrowLeft />
-      <ButtonText>Previous</ButtonText>
-    </Button>
-    <Button>
-      <PageCounter>
-        Page <PageNumber>{currentPage}</PageNumber>
-        of <PageNumber>{totalPages}</PageNumber>
-        </PageCounter>
-    </Button>
-    <Button>
+const Pagination = ({ currentPage, totalPages }) => {
+  return (
+    <Wrapper>
+      <Button>
         <ArrowLeft />
+        <ButtonText>First</ButtonText>
+      </Button>
+      <Button>
+        <ArrowLeft />
+        <ButtonText>Previous</ButtonText>
+      </Button>
+      <Button>
+        <PageCounter>
+          Page <PageNumber>{currentPage}</PageNumber>
+          of <PageNumber>{totalPages}</PageNumber>
+        </PageCounter>
+      </Button>
+      <Button>
+        <ArrowRight />
         <ButtonText>Next</ButtonText>
-    </Button>
-  </Wrapper>
+      </Button>
+      <Button>
+        <ArrowRight />
+        <ButtonText>Last</ButtonText>
+      </Button>
+    </Wrapper>
   );
 };
 
