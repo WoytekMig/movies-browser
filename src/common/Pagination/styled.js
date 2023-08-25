558b68bdf35a246202fdbce69642197076fde195
Artur Mochnia <amochnia@gmail.com>
2023-08-25 20:16:23 +0100

    added styled.js

---
+++ src/common/Pagination/styled.js
@@ -0,0 +1,31 @@
+import { styled } from "styled-components";
+import { ReactComponent as IconRight } from "../../images/vectorRight.svg";
+import { ReactComponent as IconLeft } from "../../images/vectorLeft.svg";
+
+export const Wrapper = styled.div`
+
+`;
+
+export const Button = styled.button`
+
+`;
+
+export const ButtonText = styled.span`
+
+`;
+
+export const ArrowLeft = styled(IconLeft)`
+
+`;
+
+export const ArrowRight = styled(IconRight)`
+
+`;
+
+export const PageCounter = styled.div`
+
+`;
+
+export const PageNumber = styled.span`
+
+`;
