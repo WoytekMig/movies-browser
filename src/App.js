4753146cdfd5a1773b908de352f5ff23c1a40c0c
Artur Mochnia <amochnia@gmail.com>
2023-08-25 18:25:05 +0100

    deleted file

--- src/App.js
+++ src/App.js
@@ -2,16 +2,11 @@ import MoviesBrowser from "./features/MoviesBrowser";
 import Navigation from "./common/Navigation";
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { PeopleList } from "./features/PeopleList";
 
 const App = () => (
   <BrowserRouter>
     <div>
       <Navigation />
-      <Switch>
-        <Route path="/movies" />
-        <Route path="/people" component={PeopleList} />
-      </Switch>
       <MoviesBrowser />
     </div>
   </BrowserRouter>
