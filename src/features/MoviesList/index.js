4a80de35619bce6acb877d58e51ff09df4eff482
Wojciech Migdał <122496250+WoytekMig@users.noreply.github.com>
2023-08-25 18:47:43 +0200

    first attempt to get data from API

--- src/features/MoviesList/index.js
+++ src/features/MoviesList/index.js
@@ -1,21 +1,51 @@
+import { useState } from "react";
 import MovieTile from "../../common/MovieTile";
 import { MainContainer, StyledMainHeader } from "./styled";
+import { useMoviesData } from "./useMoviesData";
 
-export const MoviesList = () => (
-    <> <MainContainer>
-        <StyledMainHeader>Popular movies</StyledMainHeader>
-
-        <MovieTile />
-        <MovieTile />
-        <MovieTile />
-        <MovieTile />
-        <MovieTile />
-        <MovieTile />
-        <MovieTile />
-        <MovieTile />
-        <MovieTile />
-    </MainContainer>
-    </>
+export const MoviesList = () => {
+    /*  const moviesData = useMoviesData(); */
 
 
-);
+    console.log(moviesData);
+
+    console.log("sprawdzamy1:", moviesData);
+    console.log("page:", moviesData.page);
+    console.log("object1:", moviesData.results[0].id);
+
+    return (
+        <> <MainContainer>
+            <StyledMainHeader>Popular movies</StyledMainHeader>
+            {/*             <button onClick={() => { fetchMoviesData(); console.log("klik !"); setTimeout(console.log(moviesData), 1000); }}>CLICK</button> */}
+
+
+
+            for (const element of moviesData.results)  {
+                <MovieTile title={moviesData.element.title}
+                    year={moviesData.element.release_date}
+
+                    rating={moviesData.element.vote_average}
+                    votes={moviesData.element.vote_count} />
+            }
+
+
+
+            {/* 
+        <MovieTile title={moviesData.title}
+            year={moviesData.release_date}
+            tag={moviesData.tag}
+            rating={moviesData.vote_average}
+            votes={moviesData.vote_count} />
+        <MovieTile />
+        <MovieTile />
+        <MovieTile />
+        <MovieTile />
+        <MovieTile />
+        <MovieTile />
+        <MovieTile />
+        <MovieTile /> */}
+        </MainContainer>
+        </>
+    )
+
+};
