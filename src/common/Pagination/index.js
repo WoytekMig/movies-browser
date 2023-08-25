febdd9bb5cb6da0356622aab5d0a609448b1e33b
Artur Mochnia <amochnia@gmail.com>
2023-08-25 21:00:32 +0100

    added buttons

--- src/common/Pagination/index.js
+++ src/common/Pagination/index.js
@@ -1,10 +1,29 @@
-import { } from "./styled";
+import { ArrowLeft, Button, ButtonText, PageCounter, PageNumber, Wrapper } from "./styled";
 
-const Pagination = () => {
-
-    return(
-
-    )
+const Pagination = ({currentPage, totalPages}) => {
+  
+  return(
+  <Wrapper>
+    <Button>
+      <ArrowLeft />
+      <ButtonText>First</ButtonText>
+    </Button>
+    <Button>
+      <ArrowLeft />
+      <ButtonText>Previous</ButtonText>
+    </Button>
+    <Button>
+      <PageCounter>
+        Page <PageNumber>{currentPage}</PageNumber>
+        of <PageNumber>{totalPages}</PageNumber>
+        </PageCounter>
+    </Button>
+    <Button>
+        <ArrowLeft />
+        <ButtonText>Next</ButtonText>
+    </Button>
+  </Wrapper>
+  );
 };
 
 export default Pagination;
