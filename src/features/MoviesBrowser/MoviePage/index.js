b8f2f3554caf6b6d3f82f1fb207001381c805841
Krystian Żybura <Krystian12324444@gmail.com>
2023-08-26 22:50:22 +0200

    MoviePageWrapper added

--- src/features/MoviesBrowser/MoviePage/index.js
+++ src/features/MoviesBrowser/MoviePage/index.js
@@ -1,7 +1,14 @@
+import MovieInfo from "./MovieInfo";
 import MovieTop from "./MovieTop";
+import { MoviePageWrapper } from "./styled";
 
 const MoviePage = () => {
-  return <MovieTop />;
+  return (
+    <MoviePageWrapper>
+      <MovieTop />
+      <MovieInfo />
+    </MoviePageWrapper>
+  );
 };
 
 export default MoviePage;
---
+++ src/features/MoviesBrowser/MoviePage/styled.js
@@ -0,0 +1,6 @@
+import { styled } from "styled-components";
+
+export const MoviePageWrapper = styled.div`
+  display: grid;
+  grid-gap: 64px;
+`;
