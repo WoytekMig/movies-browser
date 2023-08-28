5ecfd20baf8d99d30c8d2ac1c89b3179b1954e89
Krystian Żybura <Krystian12324444@gmail.com>
2023-08-28 13:58:21 +0200

    Cast and Crew added

--- src/features/MoviesBrowser/MoviePage/index.js
+++ src/features/MoviesBrowser/MoviePage/index.js
@@ -1,13 +1,18 @@
+import PeopleList from "../../../common/PeopleList";
 import MovieInfo from "./MovieInfo";
 import MovieTop from "./MovieTop";
 import { MoviePageWrapper } from "./styled";
 
 const MoviePage = () => {
   return (
-    <MoviePageWrapper>
-      <MovieTop />
-      <MovieInfo />
-    </MoviePageWrapper>
+    <>
+      <MoviePageWrapper>
+        <MovieTop />
+        <MovieInfo />
+      </MoviePageWrapper>
+      <PeopleList title={"Cast"} />
+      <PeopleList title={"Crew"} />
+    </>
   );
 };
 
