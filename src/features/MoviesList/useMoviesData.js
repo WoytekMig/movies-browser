0dfacb9241d40c20624d1ce72fb38fd9927a8cfc
Wojciech Migdał <122496250+WoytekMig@users.noreply.github.com>
2023-08-28 15:01:58 +0200

    deleted unused source

--- src/features/MoviesList/useMoviesData.js
+++ src/features/MoviesList/useMoviesData.js
@@ -1,8 +1,6 @@
 import { useState, useEffect } from "react";
 
-const nowPlaying = "https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1&region=pl";
 const discoverMovie = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc";
-
 
 const usedFetchAddress = discoverMovie;
 
