51a3f1af7f008d875c828392dded63b756336792
Krystian Żybura <Krystian12324444@gmail.com>
2023-08-28 13:56:04 +0200

    refactored, StyledContainer renamed to Main

--- src/common/Container/index.js
+++
@@ -1,7 +0,0 @@
-import { StyledContainer } from "./styled";
-
-const MainContainer = ({ children }) => (
-  <StyledContainer>{children}</StyledContainer>
-);
-
-export default MainContainer;
--- src/common/Main/index.js
+++ src/common/Main/index.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const StyledContainer = styled.div`
+export const Main = styled.div`
   max-width: 1400px;
   margin: auto;
   padding: 0 16px;
