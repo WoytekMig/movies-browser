cc2ef00e536bf03f32c170acf531d591c1dc3369
Artur Mochnia <amochnia@gmail.com>
2023-08-31 11:14:38 +0100

    added setTimeout

--- src/features/MoviesBrowser/PopularPeople/usePopularPeopleData.js
+++ src/features/MoviesBrowser/PopularPeople/usePopularPeopleData.js
@@ -8,25 +8,29 @@ export const usePopularPeopleData = (currentPage) => {
     totalPages: 1,
     currentPage: currentPage,
   });
+
   useEffect(() => {
     const fetchPopularPeopleData = async () => {
-      const url = `${ApiPopularPeople}?page=${currentPage}&api_key=${API_KEY}`;
-      try {
-        const response = await fetch(url);
-        const data = await response.json();
-        setPopularPeopleData({
-          status: "success",
-          results: data.results,
-          totalPages: data.total_pages,
-          currentPage: data.page,
-        });
-      } catch (error) {
-        setPopularPeopleData({
-          status: "error",
-        });
-      }
+      setTimeout(async () => {
+        const url = `${ApiPopularPeople}?page=${currentPage}&api_key=${API_KEY}`;
+        try {
+          const response = await fetch(url);
+          const data = await response.json();
+          setPopularPeopleData({
+            status: "success",
+            results: data.results,
+            totalPages: data.total_pages,
+            currentPage: data.page,
+          });
+        } catch (error) {
+          setPopularPeopleData({
+            status: "error",
+          });
+        }
+      }, 2000);
     };
     fetchPopularPeopleData();
   }, [currentPage]);
+
   return popularPeopleData;
 };
