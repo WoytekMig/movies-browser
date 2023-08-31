7d99ce635ba1f49d27bad6c9819b5efd04e18e77
Artur Mochnia <amochnia@gmail.com>
2023-08-31 16:42:56 +0100

    repair loading and popularpeople

--- src/common/PeopleList/index.js
+++ src/common/PeopleList/index.js
@@ -2,8 +2,9 @@ import { Main } from "../Main";
 import MainHeader from "../MainHeader";
 import { List, ListItem, StyledLink } from "./styled";
 import PersonTile from "../PersonTile";
-import Loading from "../Loading";
 import Pagination from "../Pagination";
+import Error from "../Error";
+import Loading from "../Loading";
 
 const PeopleList = ({ title, moviePage, data, currentPage, onPageChange }) => {
   const totalPages = data.totalPages || 1;
@@ -17,33 +18,35 @@ const PeopleList = ({ title, moviePage, data, currentPage, onPageChange }) => {
   return (
     <Main>
       <MainHeader title={title} />
-      <List $moviePage={moviePage}>
-        {data.status === "loading" ? (
-          <Loading />
-        ) : data.status === "error" ? (
-          <p>Something went wrong...</p>
-        ) : (
-          data.results.map((person) => (
-            <ListItem key={person.id}>
-              <StyledLink to={`/person/${person.id}`}>
-                <PersonTile
-                  picture={person.profile_path}
-                  name={person.name}
-                  role={person.character}
-                />
-              </StyledLink>
-            </ListItem>
-          ))
-        )}
-      </List>
-      <Pagination
-        currentPage={currentPage}
-        totalPages={totalPages}
-        onFirstPage={() => handlePageChange(1)}
-        onPrevPage={() => handlePageChange(currentPage - 1)}
-        onNextPage={() => handlePageChange(currentPage + 1)}
-        onLastPage={() => handlePageChange(totalPages)}
-      />
+      {data.status === "loading" ? (
+        <Loading />
+      ) : data.status === "error" ? (
+        <Error />
+      ) : (
+        <>
+          <List $moviePage={moviePage}>
+            {data.results.map((person) => (
+              <ListItem key={person.id}>
+                <StyledLink to={`/person/${person.id}`}>
+                  <PersonTile
+                    picture={person.profile_path}
+                    name={person.name}
+                    role={person.character}
+                  />
+                </StyledLink>
+              </ListItem>
+            ))}
+          </List>
+          <Pagination
+            currentPage={currentPage}
+            totalPages={totalPages}
+            onFirstPage={() => handlePageChange(1)}
+            onPrevPage={() => handlePageChange(currentPage - 1)}
+            onNextPage={() => handlePageChange(currentPage + 1)}
+            onLastPage={() => handlePageChange(totalPages)}
+          />
+        </>
+      )}
     </Main>
   );
 };
