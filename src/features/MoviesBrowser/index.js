7eb37bba3ad3fe2660bf9b76944c452a7eed8c9a
Artur Mochnia <amochnia@gmail.com>
2023-08-31 16:55:47 +0100

    removed inactive pagination

--- src/features/MoviesBrowser/index.js
+++ src/features/MoviesBrowser/index.js
@@ -1,7 +1,6 @@
 import Navigation from "../../common/Navigation";
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Pagination from "../../common/Pagination";
 import PopularPeople from "./PopularPeople";
 import MoviesList from "./MoviesList";
 
@@ -12,7 +11,6 @@ const MoviesBrowser = () => (
       <Route path="/movies" component={MoviesList} />
       <Route path="/people" component={PopularPeople} />
     </Switch>
-    <Pagination />
   </BrowserRouter>
 );
 
