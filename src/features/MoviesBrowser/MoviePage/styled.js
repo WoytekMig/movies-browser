7dc48f9b89da6dc50708d81526364eb0ff97881c
Krystian Żybura <Krystian12324444@gmail.com>
2023-08-31 11:54:10 +0200

    margin-bottom adjusted

--- src/features/MoviesBrowser/MoviePage/styled.js
+++ src/features/MoviesBrowser/MoviePage/styled.js
@@ -7,13 +7,16 @@ export const MoviePageWrapper = styled.div`
 
   @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
     grid-gap: 48px;
+    margin-bottom: 250px;
   }
 
   @media (max-width: ${({ theme }) => theme.breakpoints.defaultMobile}) {
     grid-gap: 32px;
+    margin-bottom: 150px;
   }
 
   @media (max-width: ${({ theme }) => theme.breakpoints.smallMobile}) {
     grid-gap: 16px;
+    margin-bottom: 88px;
   }
 `;
