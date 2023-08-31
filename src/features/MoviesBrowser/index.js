7d011ae3c6ed1fd75c9d8d8a9ad18fc4a82abc34
Krystian Żybura <Krystian12324444@gmail.com>
2023-08-31 12:35:44 +0200

    PopularMovies added, MovieList moved to common

--- src/features/MoviesBrowser/index.js
+++ src/features/MoviesBrowser/index.js
@@ -3,18 +3,18 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Pagination from "../../common/Pagination";
 import PopularPeople from "./PopularPeople";
-import MoviesList from "./MoviesList";
 import Profile from "./Profile";
+import PopularMovies from "./PopularMovies";
 
 const MoviesBrowser = () => (
   <BrowserRouter>
     <Navigation />
     <Profile />
-    {/* <Switch>
-      <Route path="/movies" component={MoviesList} />
+    <Switch>
+      <Route path="/movies" component={PopularMovies} />
       <Route path="/people" component={PopularPeople} />
     </Switch>
-    <Pagination /> */}
+    <Pagination />
   </BrowserRouter>
 );
 
