012ff434e8baef6a10600ae498d67b7a54b44de2
Artur Mochnia <amochnia@gmail.com>
2023-08-31 10:03:11 +0100

    update style

--- src/common/Pagination/styled.js
+++ src/common/Pagination/styled.js
@@ -1,19 +1,16 @@
 import { styled } from "styled-components";
 import { ReactComponent as IconRight } from "../../images/vectorRight.svg";
 import { ReactComponent as IconLeft } from "../../images/vectorLeft.svg";
-
 export const Wrapper = styled.div`
   display: flex;
   justify-content: center;
   margin: 40px 0 103px 0;
   gap: 12px;
-
   @media (max-width: ${({ theme }) => theme.breakpoints.defaultMobile}) {
     gap: 8px;
     margin: 16.5px 0 31.5px 0;
   }
 `;
-
 export const Button = styled.button`
   padding: 8px 16px;
   background-color: ${({ theme }) => theme.colors.pattensBlue};
@@ -24,59 +21,50 @@ export const Button = styled.button`
   gap: 6px;
   transition: 0.5s;
   cursor: pointer;
-
-  &:hover {
+  &:hover:not(:disabled) {
     filter: brightness(103%);
   }
   &:disabled {
     background-color: ${({ theme }) => theme.colors.mystic};
+    cursor: not-allowed;
+
     &:hover {
       filter: none;
     }
   }
-
   @media (max-width: ${({ theme }) => theme.breakpoints.defaultMobile}) {
     padding: 8px 12px;
     gap: 4px;
   }
 `;
-
 export const ButtonText = styled.span`
   font-size: 14px;
   line-height: 140%;
   color: ${({ theme }) => theme.colors.woodSmoke};
-
   @media (max-width: ${({ theme }) => theme.breakpoints.defaultMobile}) {
     display: none;
   }
 `;
-
 export const ArrowLeft = styled(IconLeft)`
   color: ${({ theme }) => theme.colors.scienceBlue};
-
   ${Button}:disabled & {
     color: ${({ theme }) => theme.colors.darkerGrey};
   }
-
   @media (max-width: ${({ theme }) => theme.breakpoints.defaultMobile}) {
     width: 5px;
     height: 8px;
   }
 `;
-
 export const ArrowRight = styled(IconRight)`
   color: ${({ theme }) => theme.colors.scienceBlue};
-
   ${Button}:disabled & {
     color: ${({ theme }) => theme.colors.darkerGrey};
   }
-
   @media (max-width: ${({ theme }) => theme.breakpoints.defaultMobile}) {
     width: 5px;
     height: 8px;
   }
 `;
-
 export const PageCounter = styled.div`
   display: flex;
   align-items: center;
@@ -84,14 +72,12 @@ export const PageCounter = styled.div`
   gap: 8px;
   color: ${({ theme }) => theme.colors.darkerGrey};
   margin: 0 12px 0 12px;
-
   @media (max-width: ${({ theme }) => theme.breakpoints.defaultMobile}) {
     gap: 2px;
     font-size: 10px;
     margin: 0;
   }
 `;
-
 export const PageNumber = styled.span`
   font-weight: 600;
   color: ${({ theme }) => theme.colors.woodSmoke};
