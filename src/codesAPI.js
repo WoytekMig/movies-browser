05fcf0b09dc66730732dba2f0e4522067aecbe13
Artur Mochnia <amochnia@gmail.com>
2023-09-01 09:26:20 +0100

    update codesApi

--- src/codesAPI.js
+++ src/codesAPI.js
@@ -2,3 +2,4 @@ export const API_KEY = "baca1cc9462be3b73a5e5722d6854eb5";
 export const API_URL = "https://api.themoviedb.org/3";
 export const ApiPopularPeople = "https://api.themoviedb.org/3/person/popular";
 export const PersonImageAPI = "https://image.tmdb.org/t/p/w200/";
+export const searchAPI = "https://api.themoviedb.org/3/search";
