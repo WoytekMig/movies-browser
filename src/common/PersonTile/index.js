915bc6490979e1d9f3d5013e54820338b0c2da80
Artur Mochnia <amochnia@gmail.com>
2023-09-02 22:37:37 +0100

    adding an icon when there is no photo

--- src/common/PersonTile/index.js
+++ src/common/PersonTile/index.js
@@ -1,12 +1,25 @@
 import { Name, Picture, Role, Wrapper } from "./styled";
 import { PersonImageAPI } from "../../codesAPI";
+import NoImagePerson from "../../images/noimgperson.svg";
 
-const PersonTile = ({ picture, name, role }) => (
-  <Wrapper>
-    <Picture src={`${PersonImageAPI}${picture}`} />
-    <Name>{name}</Name>
-    <Role>{role}</Role>
-  </Wrapper>
-);
+const PersonTile = ({ picture, name, role }) => {
+  if (!picture) {
+    return (
+      <Wrapper>
+        <Picture src={NoImagePerson} />
+        <Name>{name}</Name>
+        <Role>{role}</Role>
+      </Wrapper>
+    );
+  }
+
+  return (
+    <Wrapper>
+      <Picture src={`${PersonImageAPI}${picture}`} alt={name} />
+      <Name>{name}</Name>
+      <Role>{role}</Role>
+    </Wrapper>
+  );
+};
 
 export default PersonTile;
