e25804da29515b987ca63bbbf6f6e6f5e8ee7063
Krystian Żybura <Krystian12324444@gmail.com>
2023-09-03 18:39:33 +0200

    getMovieApiUrls added

--- src/codesAPI.js
+++ src/codesAPI.js
@@ -6,3 +6,10 @@ export const IMG_URL = "https://image.tmdb.org/t/p/w500/";
 
 export const getMovieImageUrl = (source) =>
   `https://image.tmdb.org/t/p/original/${source}`;
+
+export const getMovieApiUrls = (movieId) => {
+  const MOVIE_API_URL = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}`;
+  const CREDITS_API_URL = `https://api.themoviedb.org/3/movie/${movieId}/credits?language=en-US&api_key=${API_KEY}`;
+
+  return { MOVIE_API_URL, CREDITS_API_URL };
+};
