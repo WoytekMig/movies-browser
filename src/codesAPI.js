000d2712bb14072c8ff2889c52634916e69d3156
Artur Mochnia <amochnia@gmail.com>
2023-09-04 21:28:35 +0100

    deleted code

--- src/codesAPI.js
+++ src/codesAPI.js
@@ -2,7 +2,7 @@ export const API_KEY = "baca1cc9462be3b73a5e5722d6854eb5";
 export const API_URL = "https://api.themoviedb.org/3";
 export const ApiPopularPeople = "https://api.themoviedb.org/3/person/popular";
 export const PersonImageAPI = "https://image.tmdb.org/t/p/w200/";
-export const searchAPI = "https://api.themoviedb.org/3/search";
+
 export const getMovieImageUrl = (source) =>
   `https://image.tmdb.org/t/p/w1280/${source}`;
 export const getMovieApiUrls = (movieId) => {
