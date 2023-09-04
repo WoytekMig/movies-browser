d494492dad548b7623a95bdc724f6003934c895b
Wojciech Migdał <122496250+WoytekMig@users.noreply.github.com>
2023-09-04 23:19:10 +0200

    deleted unused import

--- src/common/MoviesList/styled.js
+++ src/common/MoviesList/styled.js
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import styled from "styled-components";
-/* import { Link } from "react-router-dom"; */
 
 export const MainContainer = styled.div`
   max-width: 1400px;
