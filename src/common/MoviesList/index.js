a994e3e27fe984e84fc1397a875a2c8f26309b5d
Krystian Żybura <Krystian12324444@gmail.com>
2023-09-06 17:42:15 +0200

    MoviesList adjusted

--- src/common/MoviesList/index.js
+++ src/common/MoviesList/index.js
@@ -4,27 +4,32 @@ import { MainContainer, StyledLink } from "./styled";
 import { useDispatch } from "react-redux";
 import { setMovieId } from "../../features/MoviesBrowser/moviesSlice";
 
-const MoviesList = ({ moviesData }) => {
+const MoviesList = ({ moviesData, profile }) => {
   const dispatch = useDispatch();
 
   return (
     <MainContainer>
-      {moviesData.map((movie) => (
-        <StyledLink
-          key={movie.credit_id ?? movie.id}
-          onClick={() => dispatch(setMovieId(movie.id))}
-          to={`/movie/${movie.id}`}
-        >
-          <MovieTile
-            posterPath={movie.poster_path}
-            title={movie.title}
-            year={new Date(movie.release_date).getFullYear()}
-            rating={movie.vote_average}
-            votes={movie.vote_count}
-            tag={movie.genre_ids}
-          />
-        </StyledLink>
-      ))}
+      {moviesData.map((movie) => {
+        const year = new Date(movie.release_date).getFullYear();
+        const roleAndYear = `${movie.character ?? movie.job} (${year})`;
+
+        return (
+          <StyledLink
+            key={movie.credit_id ?? movie.id}
+            onClick={() => dispatch(setMovieId(movie.id))}
+            to={`/movie/${movie.id}`}
+          >
+            <MovieTile
+              posterPath={movie.poster_path}
+              title={movie.title}
+              year={profile ? roleAndYear : year}
+              rating={movie.vote_average}
+              votes={movie.vote_count}
+              tag={movie.genre_ids}
+            />
+          </StyledLink>
+        );
+      })}
     </MainContainer>
   );
 };
