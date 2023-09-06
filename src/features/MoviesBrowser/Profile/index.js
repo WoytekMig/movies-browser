41d439edc4e46960407c24ba81861639afd67fe1
Krystian Żybura <Krystian12324444@gmail.com>
2023-09-06 13:15:53 +0200

    cast and crew added

--- src/features/MoviesBrowser/Profile/index.js
+++ src/features/MoviesBrowser/Profile/index.js
@@ -1,5 +1,4 @@
 import { Main } from "../../../common/Main";
-import MainHeader from "../../../common/MainHeader";
 import About from "./About";
 import { ProfileWrapper } from "./styled";
 import { usePersonData } from "./usePersonData";
@@ -7,11 +6,12 @@ import Error from "../../../common/Error";
 import Loading from "../../../common/Loading";
 import { useSelector } from "react-redux";
 import { selectPersonId } from "../moviesSlice";
+import MoviesList from "../../../common/MoviesList";
 
 const Profile = () => {
   const personId = useSelector(selectPersonId);
 
-  const { status, person } = usePersonData(personId);
+  const { status, person, credits } = usePersonData(personId);
 
   const modifiedBirthday = person.birthday
     ? person.birthday.split("-").reverse().join(".")
@@ -22,19 +22,27 @@ const Profile = () => {
       {status === "loading" && <Loading />}
       {status === "error" && <Error />}
       {status === "success" && (
-        <Main>
-          <ProfileWrapper>
-            <About
-              name={person.name}
-              birthdate={modifiedBirthday}
-              birthplace={person.place_of_birth}
-              description={person.biography}
-              picturePath={person.profile_path}
-            />
-          </ProfileWrapper>
-          <MainHeader profile title={"Movies - cast (4)"} />
-          <MainHeader profile title={"Movies - crew (4)"} />
-        </Main>
+        <>
+          <Main>
+            <ProfileWrapper>
+              <About
+                name={person.name}
+                birthdate={modifiedBirthday}
+                birthplace={person.place_of_birth}
+                description={person.biography}
+                picturePath={person.profile_path}
+              />
+            </ProfileWrapper>
+          </Main>
+          <MoviesList
+            moviesData={credits.cast}
+            title={`Movies - cast (${credits.cast.length})`}
+          />
+          <MoviesList
+            moviesData={credits.crew}
+            title={`Movies - crew (${credits.crew.length})`}
+          />
+        </>
       )}
     </>
   );
