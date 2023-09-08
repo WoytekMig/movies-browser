5a1d94c012bffa7aa9bde4702e2bbc8c5b3b7cec
Artur Mochnia <amochnia@gmail.com>
2023-09-08 11:04:52 +0100

    update pagination activity

--- src/features/MoviesBrowser/SearchResult/index.js
+++ src/features/MoviesBrowser/SearchResult/index.js
@@ -1,6 +1,5 @@
-
 import { useState, useEffect } from "react";
-import { useLocation, useParams } from "react-router-dom/cjs/react-router-dom";
+import { useLocation } from "react-router-dom/cjs/react-router-dom";
 import { Main } from "../../../common/Main";
 import MainHeader from "../../../common/MainHeader";
 import Loading from "../../../common/Loading";
@@ -9,17 +8,17 @@ import useSearchPeopleQuery from "./useSearchPeopleQuery";
 import PeopleList from "../../../common/PeopleList";
 import Pagination from "../../../common/Pagination";
 
-const SearchResult = ({ onPageChange }) => {
+const SearchResult = () => {
   const location = useLocation();
   const queryParam = new URLSearchParams(location.search).get("query");
   console.log("queryParam:", queryParam);
-  const currentPage = useParams().currentPage || 1;
   
+  const [ currentPage, setCurrentPage] = useState(1);
   const [isLoading, setLoading] = useState(true);
   const [dataLoaded, setDataLoaded] = useState(false);
-  
+
   const [totalPages, setTotalPages] = useState(1); // Dodana state totalPages
-  
+
   useEffect(() => {
     console.log("currentPage in SearchResult:", currentPage);
     const loadingTimeout = setTimeout(() => {
@@ -29,19 +28,20 @@ const SearchResult = ({ onPageChange }) => {
       clearTimeout(loadingTimeout);
     };
   }, [currentPage]);
+
   const { data, isError } = useSearchPeopleQuery(queryParam);
   console.log("API response:", data);
-  
+
   useEffect(() => {
     if (data !== undefined) {
       setDataLoaded(true);
       setTotalPages(data.total_pages || 1); // Ustawienie totalPages na podstawie danych z API
     }
   }, [data]);
-  const handlePageChange = (newPage) => {
+  const handleSearchPageChange = (newPage) => {
     if (newPage >= 1 && newPage <= totalPages) {
-      console.log("Changing page to:", newPage); 
-      onPageChange(newPage);
+      console.log("Changing page to:", newPage);
+      setCurrentPage(newPage);
     }
   };
   return (
@@ -58,25 +58,24 @@ const SearchResult = ({ onPageChange }) => {
       />
       {isLoading ? (
         <Loading />
-      ) : isError || !dataLoaded || !data || !data.results || data.results.length === 0 ? (
+      ) : isError ||
+        !dataLoaded ||
+        !data ||
+        !data.results ||
+        data.results.length === 0 ? (
         <NoResult query={queryParam} />
       ) : (
         <>
-          <PeopleList
-            data={data}
-            currentPage={currentPage}
-            onPageChange={handlePageChange}
-            totalPages={totalPages}
-          />
+          <PeopleList data={data} />
           <Pagination
-        currentPage={currentPage}
-        totalPages={totalPages}
-        onFirstPage={() => handlePageChange(1)}
-        onPrevPage={() => handlePageChange(currentPage - 1)}
-        onNextPage={() => handlePageChange(currentPage + 1)}
-        onLastPage={() => handlePageChange(totalPages)}
-      />
-
+            currentPage={currentPage}
+            totalPages={totalPages}
+            onPageChange={handleSearchPageChange}
+            onFirstPage={() => handleSearchPageChange(1)}
+            onPrevPage={() => handleSearchPageChange(currentPage - 1)}
+            onNextPage={() => handleSearchPageChange(currentPage + 1)}
+            onLastPage={() => handleSearchPageChange(totalPages)}
+          />
         </>
       )}
     </Main>
