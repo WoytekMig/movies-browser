f15113d497ae7db01895dc1098c7e52f4021954f
Artur Mochnia <amochnia@gmail.com>
2023-09-08 11:52:04 +0100

    added loading

--- src/features/MoviesBrowser/SearchResult/index.js
+++ src/features/MoviesBrowser/SearchResult/index.js
@@ -12,12 +12,12 @@ const SearchResult = () => {
   const location = useLocation();
   const queryParam = new URLSearchParams(location.search).get("query");
   console.log("queryParam:", queryParam);
-  
-  const [ currentPage, setCurrentPage] = useState(1);
+
+  const [currentPage, setCurrentPage] = useState(1);
   const [isLoading, setLoading] = useState(true);
   const [dataLoaded, setDataLoaded] = useState(false);
 
-  const [totalPages, setTotalPages] = useState(1); // Dodana state totalPages
+  const [totalPages, setTotalPages] = useState(1);
 
   useEffect(() => {
     console.log("currentPage in SearchResult:", currentPage);
@@ -35,13 +35,14 @@ const SearchResult = () => {
   useEffect(() => {
     if (data !== undefined) {
       setDataLoaded(true);
-      setTotalPages(data.total_pages || 1); // Ustawienie totalPages na podstawie danych z API
+      setTotalPages(data.total_pages || 1);
     }
   }, [data]);
   const handleSearchPageChange = (newPage) => {
     if (newPage >= 1 && newPage <= totalPages) {
       console.log("Changing page to:", newPage);
       setCurrentPage(newPage);
+      setLoading(true);
     }
   };
   return (
@@ -66,7 +67,7 @@ const SearchResult = () => {
         <NoResult query={queryParam} />
       ) : (
         <>
-          <PeopleList data={data} />
+          <PeopleList data={data} currentPage={currentPage} />
           <Pagination
             currentPage={currentPage}
             totalPages={totalPages}
