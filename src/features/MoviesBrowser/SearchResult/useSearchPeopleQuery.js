ce1e501cb74a3ecaaac42aff81dd27dd54566978
Wojciech Migdał <122496250+WoytekMig@users.noreply.github.com>
2023-09-11 19:38:09 +0200

    improved useSearchPeopleQuery

--- src/features/MoviesBrowser/SearchResult/useSearchPeopleQuery.js
+++ src/features/MoviesBrowser/SearchResult/useSearchPeopleQuery.js
@@ -2,7 +2,7 @@ import { useQuery } from "react-query";
 import searchPeople from "./getSearch";
 
 const useSearchPeopleQuery = (query, currentPage) => {
-  return useQuery(["searchPeople", query], () =>
+  return useQuery(["searchPeople", query, currentPage], () =>
     searchPeople({ query, page: currentPage })
   );
 };
