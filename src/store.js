ebb8c02a9fb46ece9e6746f808021c60cf04b59d
Krystian Żybura <Krystian12324444@gmail.com>
2023-09-12 17:19:02 +0200

    redux responsive implemented

--- src/store.js
+++ src/store.js
@@ -2,14 +2,23 @@ import { configureStore } from "@reduxjs/toolkit";
 import moviesReducer from "./features/MoviesBrowser/moviesSlice";
 import createSagaMiddleware from "@redux-saga/core";
 import { moviesSaga } from "./features/MoviesBrowser/moviesSaga";
+import { responsiveStoreEnhancer } from "redux-responsive";
+import { createResponsiveStateReducer } from "redux-responsive";
 
 const sagaMiddleware = createSagaMiddleware();
+
+const media = {
+  smallMobile: 450,
+  defaultMobile: 767,
+};
 
 const store = configureStore({
   reducer: {
     movies: moviesReducer,
+    responsive: createResponsiveStateReducer(media),
   },
   middleware: [sagaMiddleware],
+  enhancers: [responsiveStoreEnhancer],
 });
 
 sagaMiddleware.run(moviesSaga);
