e65dcb09b0724f44ff33f5cff2a3e888eca59237
Wojciech Migdał <122496250+WoytekMig@users.noreply.github.com>
2023-09-15 17:11:29 +0200

    updated name of component

---
+++ src/features/MoviesBrowser/SearchResult/useSearchQuery.js
@@ -0,0 +1,14 @@
+import { useQuery } from "react-query";
+import fetchSearchToAPI from "./getSearch";
+
+const useSearchQuery = (query, currentPage, topic) => {
+  const fetchSearchData = async () => {
+    await new Promise((resolve) => setTimeout(resolve, 300));
+
+    return fetchSearchToAPI({ query, page: currentPage, topic });
+  };
+
+  return useQuery(["search", query, currentPage], fetchSearchData);
+};
+
+export default useSearchQuery;
