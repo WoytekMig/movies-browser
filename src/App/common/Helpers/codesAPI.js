f8d0acffc125a5a88d161089b5b965d5d2d61cf5
Krystian Żybura <Krystian12324444@gmail.com>
2023-09-22 12:48:14 +0200

    getDetailsApiUrls added

--- src/App/common/Helpers/codesAPI.js
+++ src/App/common/Helpers/codesAPI.js
@@ -7,16 +7,11 @@ export const IMG_URL = "https://image.tmdb.org/t/p/w500/";
 export const getMovieImageUrl = (source) =>
   `https://image.tmdb.org/t/p/w1280/${source}`;
 
-export const getMovieApiUrls = (movieId) => {
-  const MOVIE_API_URL = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}`;
-  const CREDITS_API_URL = `https://api.themoviedb.org/3/movie/${movieId}/credits?language=en-US&api_key=${API_KEY}`;
+export const getDetailsApiUrls = (id, type) => {
+  const DETAILS_API_URL = `https://api.themoviedb.org/3/${type}/${id}?api_key=${API_KEY}`;
+  const CREDITS_API_URL = `https://api.themoviedb.org/3/${type}/${id}/${
+    type === "movie" ? "credits" : "movie_credits"
+  }?api_key=${API_KEY}`;
 
-  return { MOVIE_API_URL, CREDITS_API_URL };
-};
-
-export const getPersonApiUrls = (personId) => {
-  const PERSON_API_URL = `https://api.themoviedb.org/3/person/${personId}?api_key=${API_KEY}`;
-  const CREDITS_API_URL = `https://api.themoviedb.org/3/person/${personId}/movie_credits?api_key=${API_KEY}`;
-
-  return { PERSON_API_URL, CREDITS_API_URL };
+  return { DETAILS_API_URL, CREDITS_API_URL };
 };
